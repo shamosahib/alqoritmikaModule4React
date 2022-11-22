52ef8d1d8c58cb30e295ba08990808cae36b2101
Shamo Naghiyev <nagyev2@gmail.com>
2022-11-22 14:55:04 +0400

    padding remove

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Profile from './Profile';
 class App extends Component {
   render(){
     return(
-    <Profile  firstName='Shamo' lastName='Naghiyev' email='example@gmail.com' personalNum='050 000 00 00' adress='Narimanov Ms'/>
+      <div className='App'>
+         <Profile  firstName='Shamo' lastName='Naghiyev' email='example@gmail.com' personalNum='050 000 00 00' adress='Narimanov Ms'/>
+         <Profile  firstName='Shamo' lastName='Naghiyev' email='example@gmail.com' personalNum='050 000 00 00' adress='Narimanov Ms'/>
+      </div>
+   
     )
   }
 }
