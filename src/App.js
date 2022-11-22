bd68042b75308fd1fddbf39f2348853043547c74
Shamo Naghiyev <nagyev2@gmail.com>
2022-11-22 15:06:55 +0400

    Complete All Tasks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, {Component} from 'react';
-import './App.css';
-import Profile from './Profile';
+import React, { Component } from "react";
+import Wine from "./Wine";
 
-
-class App extends Component {
-  render(){
-    return(
-      <div className='App'>
-         <Profile  firstName='Shamo' lastName='Naghiyev' email='example@gmail.com' personalNum='050 000 00 00' adress='Narimanov Ms'/>
-         <Profile  firstName='Shamo' lastName='Naghiyev' email='example@gmail.com' personalNum='050 000 00 00' adress='Narimanov Ms'/>
-      </div>
-   
-    )
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Wine
+          title="Hungarian Wine"
+          description="10-year-old natural Hungarian cognac"
+          rating="6.6"
+          liked
+        />
+        <Wine
+          title="Hungarian Wine"
+          description="10-year-old natural Hungarian cognac"
+          rating="6.6"
+          liked={false}
+        />
+      </>
+    );
   }
 }
-
-export default App;
