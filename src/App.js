267abe6fd6adc7bf9d95efb40746c4bf8741d295
Shamo Naghiyev <nagyev2@gmail.com>
2022-11-28 12:32:15 +0400

    Delete app

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <Wine
-          title="Hungarian Wine"
-          description="10-year-old natural Hungarian cognac"
-          rating="6.6"
-          liked
-        />
-        <Wine
-          title="Hungarian Wine"
-          description="10-year-old natural Hungarian cognac"
-          rating="6.6"
-          liked={false}
-        />
+        
       </>
     );
   }
