98ccf63bf807d7bc0d3cbf01139debbd877a2e6e
Shamo Naghiyev <nagyev2@gmail.com>
2022-11-30 12:11:54 +0400

    Delete old files and use this template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Splide from './Splide';
 
 export default class App extends React.Component {
  
   render() {
     return (
       <div className='container'>
-        <Splide/>
+        
       </div>
     );
   }
