84f44226310561eff952fe52f9138f6c9eb84d6c
Shamo Naghiyev <nagyev2@gmail.com>
2022-11-30 13:09:52 +0400

    Presentation task example complete

--- src/App.js
+++ src/App.js
@@ -5,27 +5,47 @@ export default class App extends React.Component {
  
   state ={
     user: [
-      {name:'User1', age: '19', position: 'Admin',},
-      {name:'User1', age: '19', position: 'Admin',},
-      {name:'User1', age: '19', position: 'Admin',},
-      {name:'User1', age: '19', position: 'Admin',},
-      {name:'User1', age: '19', position: 'Admin',}
+      {id:'1', name:'Nikon', age: '19', position: 'Admin',},
+      {id:'2', name:'Canon', age: '19', position: 'Admin',},
+      {id:'3', name:'Sony', age: '19', position: 'Admin',},
+      {id:'4', name:'Fuji', age: '19', position: 'Admin',},
+      {id:'5', name:'Panasonic', age: '19', position: 'Admin',},
+      
       
     ]
   }
 
   userAdder = (index) =>{
-    const copyUser = {...this.state.user}
-    console.log(copyUser)
+    const copyUser = this.state.user;
+    const newArr = [
+      ...copyUser.slice(0,index), {name: "Apple", age:'36', position:'CEO'},
+      ...copyUser.slice(index)
+    ]
+    this.setState({user: [...newArr]})
   }
 
+  deleteUser = (id) => {
+    const copy = this.state.user;
+    const newArr = copy.filter(item=> item.id !== id);
+    this.setState({user: newArr});
+  }
   render() {
     return (
-      <div className='container'>
+      <div>
+        <div className='container'>
           {this.state.user.map(item=>{
-            return <User {...item}/>
+            return(
+              <div className='userContainer'>
+                <User {...item}/>
+                <button type='button' onClick={() => this.deleteUser(item.id)}>Delete</button>
+              </div>
+               
+            ) 
           })}
+        </div>
+        <button type='button' onClick={() => this.userAdder(2)}>Add</button>
       </div>
+      
     );
   }
  }
